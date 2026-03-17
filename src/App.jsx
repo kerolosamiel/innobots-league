8b5492feb04d52c920a6400af666893539ee352b
Kerolos Amiel <kerolosamiel4@gmail.com>
2026-03-17 15:12:58 +0200

    refactor: Create the main pages and Handle the routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import CommingSoon from "./pages/ComingSoon";
+import NotFound from "./pages/NotFound";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<CommingSoon />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
